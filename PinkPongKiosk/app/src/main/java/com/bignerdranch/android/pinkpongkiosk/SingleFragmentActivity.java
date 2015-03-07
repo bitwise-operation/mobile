521a29c6878513daaff7d39c6ac03635cd438808
Kristin Marsicano <kristin@bignerdranch.com>
2015-03-06 23:48:00 -0500

    customize toolbar

--- PinkPongKiosk/app/src/main/java/com/bignerdranch/android/pinkpongkiosk/SingleFragmentActivity.java
+++ PinkPongKiosk/app/src/main/java/com/bignerdranch/android/pinkpongkiosk/SingleFragmentActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 
 
 public abstract class SingleFragmentActivity extends ActionBarActivity {
@@ -17,6 +18,9 @@ public abstract class SingleFragmentActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_fragment);
 
+        Toolbar actionBar = (Toolbar) findViewById(R.id.actionBar);
+        setSupportActionBar(actionBar);
+        
         FragmentManager fragmentManager = getSupportFragmentManager();
         Fragment fragment = fragmentManager.findFragmentById(R.id.activity_single_fragment_fragment_container);
 
